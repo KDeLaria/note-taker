cf19a8b269641b57feb641def8efc74093f41750
KDeLaria <149177749+KDeLaria@users.noreply.github.com>
2024-02-02 18:05:35 -0600

    added commented function to the getDatabase function

--- middleware/getDatabase.js
+++ middleware/getDatabase.js
@@ -1,5 +1,12 @@
 const notes = require("../db/db.json");
+// const fs = require("fs");
 
+// function getDatabase(req, res, next) {
+//   fs.readFile("../db/db.json", 'utf8', (err, data) => {
+//   req.database = JSON.parse(data); 
+//   next();
+//   });
+// }
 function getDatabase(req, res, next){
   req.database = notes; 
   next();
